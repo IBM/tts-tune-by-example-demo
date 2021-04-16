81e65a5b0ba8a3faa4035f841c52092770c23b5b
marconoel <marco.noel@ca.ibm.com>
2021-04-16 10:08:04 -0400

    Update 7-createTTSSpeakerModel.sh

--- 7-createTTSSpeakerModel.sh
+++ 7-createTTSSpeakerModel.sh
@@ -5,4 +5,4 @@ var=${1:?You have to provide a WAV audio filename with no spaces as an argument}
 # The Speaker model id will be stored in the JSON file with the same name as the audio file
 bname=$(basename "$1" .wav) &&
 echo "Creating speaker id for" $1 " ............." &&
-curl -X POST -u $useCred --header "Content-Type: audio/wav" --data-binary "@$1" "$url/v1/speakers/$bname" > $bname.json
+curl -X POST -u $useCred --header "Content-Type: audio/wav" --data-binary "@$1" "$url/v1/speakers?speaker_name=$bname" > $bname.json
